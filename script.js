c16e20801f65162224fad9816cdb7151222183ee
mrpatra08 <patrasahil013@gmail.com>
2024-11-23 22:07:35 +0530

    completed page2 of lazarev

--- script.js
+++ script.js
@@ -37,5 +37,30 @@ nav.addEventListener("mouseleave",function(){     //adding event in the nav bar
     })
 }) 
 }
-
 navAnimation()
+
+function page2Animation(){
+
+var relem = document.querySelectorAll(".right-elem")
+relem.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(elem.childNodes[3],{      //NodeList(5) [text, h2, text, img, text]
+            opacity: 1,
+            scale: 1
+        }) 
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(elem.childNodes[3],{      //NodeList(5) [text, h2, text, img, text]
+            opacity: 0,
+            scale: 0
+        }) 
+    })
+    elem.addEventListener("mousemove",function(dets){
+        gsap.to(elem.childNodes[3], {
+            x:dets.x - elem.getBoundingClientRect().x-50 ,
+            y:dets.y - elem.getBoundingClientRect().y-170
+        })
+    })
+})
+}
+page2Animation()
